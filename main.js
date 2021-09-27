743a1f308e4ff2abeffad57c0e16c004cfd2eecd
Kamaldeen Aiyeleso <Aiyelesok@gmail.com>
2021-09-27 19:45:41 +0000

    First push

---
+++ main.js
@@ -0,0 +1,51 @@
+const menuItems = document.getElementById('menuItems');
+const mybutton = document.getElementById('myBtn');
+// Modal variables
+const modal = document.getElementById('myModal');
+const closemodal = document.getElementsByClassName('close')[0];
+const btnModal = document.getElementById('openModal');
+
+//User clicks on "Return policy" and invokes the modal page
+btnModal.onclick = () => {
+  modal.style.display = 'block';
+};
+
+// User action on clicking on the close button on modal
+closemodal.onclick = () => {
+  modal.style.display = 'none';
+};
+
+//User action on clicking anywhere outside of the modal page
+
+
+
+menuItems.style.maxHeight = '0px';
+
+window.onload = function () {
+  scrollFunction();
+};
+
+const menuToggle = () => {
+  if (menuItems.style.maxHeight == '0px') {
+    menuItems.style.maxHeight = '200px';
+  } else {
+    menuItems.style.maxHeight = '0px';
+  }
+};
+
+window.onscroll = () => {
+  scrollFunction();
+};
+
+const scrollFunction = () => {
+  if (document.body.scrollTop > 10 || document.documentElement.scrollTop > 10) {
+    mybutton.style.display = 'block';
+  } else {
+    mybutton.style.display = 'none';
+  }
+};
+
+const topFunction = () => {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+};
